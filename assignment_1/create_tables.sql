4ebb44a6357f3e031fc589c1d7bdd0976cedfeb2
Chris Primis <93949726+cprimis@users.noreply.github.com>
2023-09-06 18:11:21 -0400

    Update create_tables.sql
    Changed the amount of info that can be stored in an ID from 3 -> 4

--- assignment_1/create_tables.sql
+++ assignment_1/create_tables.sql
@@ -1,7 +1,7 @@
 /*=====================Students table===================*/
 CREATE TABLE Students
 (
-    StudentID char(3) NOT NULL PRIMARY KEY,
+    StudentID char(4) NOT NULL PRIMARY KEY,
     FName nvarchar(30) NOT NULL ,
     LName nvarchar(30) NOT NULL ,
     DoB DATE NOT NULL , /* YYYY-MM-DD*/
@@ -10,7 +10,7 @@ CREATE TABLE Students
 /*=====================Courses table===================*/
 CREATE TABLE Courses
 (
-    Course_ID char(3) NOT NULL PRIMARY KEY,
+    Course_ID char(4) NOT NULL PRIMARY KEY,
     CName nvarchar(25) NOT NULL ,
     Credits INT /* */
 );
@@ -18,8 +18,8 @@ CREATE TABLE Courses
 CREATE TABLE Enrollment
 (
 	EnrollmentID char(4) PRIMARY KEY,
-	StudentID char(3),
-	Course_ID char(3),
+	StudentID char(4),
+	Course_ID char(4),
 	FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
 	FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID),
     /* Add StudentID as a foreign key later*/
@@ -41,7 +41,7 @@ CREATE TABLE Teaches
 	TeachID char(4) PRIMARY KEY,
 	ProfessorID char(4),
 	FOREIGN KEY (ProfessorID) REFERENCES Professors(ProfessorID),
-	Course_ID char(3),
+	Course_ID char(4),
 	FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
     /* Add ProfessorID as a foreign key later*/
     /* Add CourseID as a foreign key later*/
