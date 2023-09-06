aa0e7ae564543c5eee0a77b6dfbda254441b5b8d
Chris Primis <93949726+cprimis@users.noreply.github.com>
2023-09-06 12:08:56 -0400

    I messed up the file but now it works

--- assignment_1.sql
+++ assignment_1.sql
@@ -1,31 +1,33 @@
-CREATE DATABASE Assignment_1
-USE DATABASE Assignment_1
 /*=====================Students table===================*/
 CREATE TABLE Students
 (
-student_id char(3) NOT NULL PRIMARY KEY,
-FName nvarchar(30) NOT NULL ,
-LName nvarchar(30) NOT NULL ,
-DoB DATE NOT NULL , /* YYYY-MM-DD*/
-Major nvarchar(30) NOT NULL
+    StudentID char(3) NOT NULL PRIMARY KEY,
+    FName nvarchar(30) NOT NULL ,
+    LName nvarchar(30) NOT NULL ,
+    DoB DATE NOT NULL , /* YYYY-MM-DD*/
+    Major nvarchar(30) NOT NULL
 );
 /*=====================Courses table===================*/
 CREATE TABLE Courses
 (
-Course_ID char(3) NOT NULL PRIMARY KEY,
-CName nvarchar(25) NOT NULL ,
-Credits INT /* */
+    Course_ID char(3) NOT NULL PRIMARY KEY,
+    CName nvarchar(25) NOT NULL ,
+    Credits INT /* */
 );
 /*=====================Enrollment table===================*/
 CREATE TABLE Enrollment
 (
 	EnrollmentID char(4) PRIMARY KEY,
+	StudentID char(3),
+	Course_ID char(3),
+	FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+	FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID),
     /* Add StudentID as a foreign key later*/
     /* Add CourseID as a foreign key later*/
 	grade nvarchar(30) NOT NULL
 );
 /*=====================Professors table===================*/
-CREATE TABLE Enrollment
+CREATE TABLE Professors
 (
 	ProfessorID char(4) PRIMARY KEY,
     FName nvarchar(30) NOT NULL ,
@@ -37,24 +39,28 @@ CREATE TABLE Enrollment
 CREATE TABLE Teaches
 (
 	TeachID char(4) PRIMARY KEY,
+	ProfessorID char(4),
+	FOREIGN KEY (ProfessorID) REFERENCES Professors(ProfessorID),
+	Course_ID char(3),
+	FOREIGN KEY (Course_ID) REFERENCES Courses(Course_ID)
     /* Add ProfessorID as a foreign key later*/
     /* Add CourseID as a foreign key later*/
 );
 
 /*===================== Foreign Key===================*/
 /*==========Enrollment=========*/
+/*
 ALTER TABLE Enrollment
-ADD CONSTRAINT fk_StudentID
-FOREIGN KEY (StudentID) REFERENCES Students (StudentID);
+ADD FOREIGN KEY (StudentID) REFERENCES Students(StudentID);
 
 ALTER TABLE Enrollment
-ADD CONSTRAINT fk_CourseID
-FOREIGN KEY (CourseID) REFERENCES Courses (CourseID);
+ADD FOREIGN KEY (CourseID) REFERENCES Courses(CourseID);
+*/
 /*=========Teaches===========*/
+/*
 ALTER TABLE Teaches 
-ADD CONSTRAINT fk_ProfessorID
-FOREIGN KEY (ProfessorID) REFERENCES Professors (ProfessorID);
+ADD FOREIGN KEY (ProfessorID) REFERENCES Professors(ProfessorID);
 
 ALTER TABLE Teaches 
-ADD CONSTRAINT fk_CourseID
-FOREIGN KEY (CourseID) REFERENCES Courses (CourseID);
+ADD FOREIGN KEY (CourseID) REFERENCES Courses(CourseID);
+*/
