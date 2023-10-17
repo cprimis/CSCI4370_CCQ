1d3c940401dcf474864f0e52afeb5514b63c9c41
pcm35221 <105086011+pcm35221@users.noreply.github.com>
2023-10-17 03:37:52 -0400

    Updated user leaderboard query

--- project_2/sql/queries.sql
+++ project_2/sql/queries.sql
@@ -4,20 +4,7 @@ SELECT movie_name FROM Ratings WHERE
 -- Delete
 
 -- User Leaderboard
-ALTER TABLE Ratings ADD COLUMN num_of_reviews INT;
-
-UPDATE Ratings AS r 
-JOIN (     
-SELECT DISTINCT username, COUNT(username) AS review_count     
-FROM Ratings     
-GROUP BY username 
-ORDER BY username ) 
-AS subquery ON r.username = subquery.username 
-SET r.num_of_reviews = subquery.review_count;
-
-SELECT DISTINCT username, num_of_reviews from Ratings ORDER BY num_of_reviews DESC LIMIT 10;
-
-SELECT username, COUNT(*) AS "review_count" FROM Ratings GROUP BY username ORDER BY COUNT(*) DESC LIMIT 10
+SELECT username, COUNT(*) AS "review_count" FROM Ratings GROUP BY username ORDER BY COUNT(*) DESC LIMIT 10;
 
 -- Reviews by Directors
 SELECT * FROM 
