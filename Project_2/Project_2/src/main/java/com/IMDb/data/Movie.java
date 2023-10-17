86c2b17a78dcaa4b9bce56c13d79325be805075a
Chris Primis <93949726+cprimis@users.noreply.github.com>
2023-10-17 11:14:37 -0400

    All Movie names added to insert movie drop down list

---
+++ Project_2/Project_2/src/main/java/com/IMDb/data/Movie.java
@@ -0,0 +1,23 @@
+package com.IMDb.data;
+
+public class Movie {
+	private int rank;
+	private String movie_name;	
+	private String release_year;
+	private float imdb_rating;
+	private int duration;
+	private String genre;
+	
+	public Movie(int rank, String movie_name, String release_year,
+			float imdb_rating, int duration, String genre) {
+		this.rank = rank;
+		this.movie_name = movie_name;		
+		this.release_year = release_year;
+		this.imdb_rating = imdb_rating;
+		this.duration = duration;
+		this.genre = genre;
+	} // Movie
+
+
+
+} // Movie
---
+++ Project_2/Project_2/src/main/java/com/IMDb/data/Rating.java
@@ -0,0 +1,21 @@
+package com.IMDb.data;
+
+public class Rating {
+    private int rating_id;
+    private String real_name;
+    private String username;
+    private String movie_name;
+    private double user_rating;
+    private String user_review;
+
+    public Rating(int rating_id, String real_name, String username, 
+            String movie_name, double user_rating, String user_review) {
+        this.rating_id = rating_id;
+        this.real_name = real_name;
+        this.username = username;
+        this.movie_name = movie_name;
+        this.user_rating = user_rating;
+        this.user_review = user_review;
+    } // Rating
+    
+} // Rating
