f01f536b03cd58cf46aca131d36bceb9b0b95241
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-10-17 16:18:37 -0400

    Update MyErrorController.java

--- Project_2/src/main/java/com/IMDb/controller/MyErrorController.java
+++ Project_2/src/main/java/com/IMDb/controller/MyErrorController.java
@@ -1,10 +1,8 @@
 package com.IMDb.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -15,18 +13,11 @@ public class MyErrorController implements ErrorController {
     public String handleError(HttpServletRequest request) {
         // get error status
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
+ 
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
 
-            // display specific error page
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "404 Error. Please try again.";
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "500 Error. Please try again.";
-            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "403 Error. Please try again.";
-            }
+            return statusCode + " Error. Please try again.";
         }
 
         // display generic error
