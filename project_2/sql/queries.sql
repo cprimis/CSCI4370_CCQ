2e484174ba2617299fd222b2d8b55ad9578c10f0
jhaudey <95141448+jhaudey@users.noreply.github.com>
2023-10-16 20:28:50 -0400

    Update queries.sql

--- project_2/sql/queries.sql
+++ project_2/sql/queries.sql
@@ -1,1 +1,12 @@
+-- Insert
+SELECT movie_name FROM Ratings WHERE 
 
+-- Delete
+
+-- User Leaderboard
+
+-- Reviews by Directors
+SELECT * FROM 
+SELECT DISTINCT rating_id, user_rating, user_raview, Ratings.movie_name,  FROM Ratings NATURAL JOIN Movie_Directors WHERE 
+
+-- Reviews by Actors
