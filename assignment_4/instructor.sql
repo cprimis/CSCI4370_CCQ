b2adda93a81ab3b0776a019b75d6f1aefdc81b8d
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-11-04 10:40:39 -0400

    Create instructor.sql

---
+++ assignment_4/instructor.sql
@@ -0,0 +1,19 @@
+create table instructor
+  (ID int,
+  name varchar(20) not null,
+  dept_name varchar(20),
+  salary numeric(8,2),
+  primary key (ID));
+
+insert into instructor values (33456, 'Gold', 'Physics', '87000');
+insert into instructor values (32343, 'El Said', 'History', '60000');
+insert into instructor values (22222, 'Einstein', 'Physics', '95000');
+insert into instructor values (15151, 'Mozart', 'Music', '40000');
+insert into instructor values (83821, 'Brandt', 'Comp. Sci.', '92000');
+insert into instructor values (76543, 'Singh', 'Finance', '80000');
+insert into instructor values (98345, 'Kim', 'Elec. Eng.', '80000');
+insert into instructor values (12121, 'Wu', 'Finance', '90000');
+insert into instructor values (10101, 'Srinivasan', 'Comp. Sci.', '65000');
+insert into instructor values (45565, 'Katz', 'Comp. Sci.', '75000');
+insert into instructor values (76766, 'Crick', 'Biology', '72000');
+insert into instructor values (58583, 'Califieri', 'History', '62000');
