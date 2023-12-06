f095a1538c9ef679ff3c88c321808c6fdc31d007
pcm35221 <105086011+pcm35221@users.noreply.github.com>
2023-12-06 15:13:20 -0500

    Update queries.sql

--- Final_Project/queries.sql
+++ Final_Project/queries.sql
@@ -1,14 +1,16 @@
--- Top 10 tracks with highest Tempo (from Musicality)
+-- Top 10 tracks with highest Tempo (from Musicality Table)
 SELECT Track.Track_ID, Track.Track, Musicality.Tempo, Musicality.Musicality_Key
   FROM Track 
   JOIN Musicality ON Track.Track_ID = Musicality.Track_ID 
   ORDER BY Musicality.Tempo DESC 
   LIMIT 10;
+
 -- Mean Tempo
 SELECT AVG(Tempo) as Mean_Tempo 
   FROM Musicality;
 
--- Top 10 Tracks with Highest Energy (from Energy)
+
+-- Top 10 Tracks with Highest Energy (from Energy Table)
 SELECT Track.Track_ID, Track.Track, Energy.Energy, Energy.Danceability
   FROM Track 
   JOIN Energy ON Track.Track_ID = Energy.Track_ID 
@@ -19,7 +21,8 @@ SELECT Track.Track_ID, Track.Track, Energy.Energy, Energy.Danceability
 SELECT AVG(Energy) as Mean_Energy
   FROM Energy;
 
--- Top 10 Tracks with Highest Danceability (from Energy)
+
+-- Top 10 Tracks with Highest Danceability (from Energy Table)
 SELECT Track.Track_ID, Track.Track, Energy.Energy, Energy.Danceability
   FROM Track 
   JOIN Energy ON Track.Track_ID = Energy.Track_ID 
@@ -30,7 +33,20 @@ SELECT Track.Track_ID, Track.Track, Energy.Energy, Energy.Danceability
 SELECT AVG(Danceability) as Mean_Danceability
   FROM Danceability;
 
---  Top 10 Tracks with Highest Valence (from Human connection)
+-- Top 10 Tracks with Highest Energy and Danceability Metric together, taken as average of both (From Energy Table)
+SELECT Track.Track_ID, Track.Track, Energy.Energy, Energy.Danceability, 
+  (Energy + Danceability) / 2 AS Energy_Danceability
+FROM Track
+JOIN Energy ON Track.Track_ID = Energy.Track_ID
+ORDER BY Energy_Danceability DESC
+LIMIT 10;
+
+-- Mean Energy_Danceability
+Select AVG((Energy + Danceability) / 2) as Mean_Energy_Danceability
+  FROM Track JOIN Energy ON Track.Track_ID = Energy.Track_ID;
+
+
+--  Top 10 Tracks with Highest Valence (from Human_Connection Table)
 SELECT Track.Track_ID, Track.Track, Human_Connection.Liveness, Human_Connection.Valence
   FROM Track 
   JOIN Human_Connection ON Track.Track_ID = Human_Connection.Track_ID 
@@ -41,20 +57,14 @@ SELECT Track.Track_ID, Track.Track, Human_Connection.Liveness, Human_Connection.
 SELECT AVG(Valence) as Mean_Valence
   FROM Human_Connection;
 
---  Top 10 Tracks with Highest Spoken Volume (from Spoken_Volume)
+
+--  Top 10 Tracks with Highest Loudness (from Spoken_Volume Table)
 SELECT Track.Track_ID, Track.Track, Spoken_Volume.Loudness, Spoken_Volume.Instrumentalness, Spoken_Volume.Speechiness, Spoken_Volume.Acousticness
   FROM Track 
   JOIN Spoken_Volume ON Track.Track_ID = Spoken_Volume.Track_ID 
   ORDER BY Spoken_Volume.Loudness DESC 
   LIMIT 10;
 
+-- Mean Loudness
 SELECT AVG(Loudness) as Mean_Loudness
   FROM Spoken_Volume;
-
-SELECT Track.Track_ID, Track.Track, Energy.Energy, Energy.Danceability, 
-  (Energy + Danceability) / 2 AS Energy_Danceability
-FROM Track
-JOIN Energy ON Track.Track_ID = Energy.Track_ID
-ORDER BY Energy_Danceability DESC
-LIMIT 10;
-
