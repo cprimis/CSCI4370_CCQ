9d418eab1aa7609621fd2985d8b1cc6e1d662f88
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-12-06 17:12:29 -0500

    Update Leader.java

--- Final_Project/final_project/src/main/java/com/Spotify/data/Leader.java
+++ Final_Project/final_project/src/main/java/com/Spotify/data/Leader.java
@@ -2,21 +2,21 @@ package com.Spotify.data;
 
 public class Leader {
     private int rank;
-    private String username;
-    private int review_count;
+    private String Album_Name;
+    private int TrackCount;
 
-    public Leader(int rank, String username, int review_count) {
+    public Leader(int rank, String Album_Name, int TrackCount) {
         this.rank = rank;
-        this.username = username;
-        this.review_count = review_count;
+        this.Album_Name = Album_Name;
+        this.TrackCount = TrackCount;
     }
     
     @Override
     public String toString() {
         return "Leader{" +
                 "rank=" + rank +
-                ", username='" + username + '\'' +
-                ", review_count=" + review_count +
+                ", Album_Name='" + Album_Name + '\'' +
+                ", review_count=" + TrackCount +
                 '}';
     } // toString
 
