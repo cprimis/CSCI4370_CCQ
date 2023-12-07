6d762076d763a4a14913137350fc6c470690592c
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-12-07 16:02:00 -0500

    Update EnergyController.java

--- Final_Project/final_project/src/main/java/com/Spotify/controller/EnergyController.java
+++ Final_Project/final_project/src/main/java/com/Spotify/controller/EnergyController.java
@@ -27,7 +27,8 @@ public class EnergyController {
                     + "FROM Track "
                     + "JOIN Energy ON Track.Track_ID = Energy.Track_ID "
                     + "JOIN Album ON Track.Album_ID = Album.Album_ID "
-                    + "JOIN Artist on Album.Artist_ID = Artist.Artist_ID " + "ORDER BY Energy.Energy DESC "
+                    + "JOIN Artist on Album.Artist_ID = Artist.Artist_ID " 
+                    + "ORDER BY Energy.Energy DESC "
                     + "LIMIT 10";
 
             ResultSet rs = stmt.executeQuery(energyQ);
@@ -52,7 +53,8 @@ public class EnergyController {
                     + "FROM Track "
                     + "JOIN Energy ON Track.Track_ID = Energy.Track_ID "
                     + "JOIN Album ON Track.Album_ID = Album.Album_ID "
-                    + "JOIN Artist on Album.Artist_ID = Artist.Artist_ID " + "ORDER BY Energy.Danceability DESC "
+                    + "JOIN Artist on Album.Artist_ID = Artist.Artist_ID " 
+                    + "ORDER BY Energy.Danceability DESC "
                     + "LIMIT 10";
             ResultSet rs3 = stmt.executeQuery(danceabilityQ);
             List<Stat> danceabilities = new ArrayList<>();
@@ -72,6 +74,30 @@ public class EnergyController {
             double danceabilityAvg = rs4.getDouble("Mean_Danceability");
             mv.addObject("DanceabilityAvg", danceabilityAvg);
 
+            String energyDanceabilityQ = "SELECT Track.Track, Artist_Name, (Energy + Danceability) / 2 AS Energy_Danceability "
+                    + "FROM Track "
+                    + "JOIN Energy ON Track.Track_ID = Energy.Track_ID "
+                    + "JOIN Album ON Track.Album_ID = Album.Album_ID "
+                    + "JOIN Artist on Album.Artist_ID = Artist.Artist_ID "
+                    + "ORDER BY Energy_Danceability DESC "
+                    + "LIMIT 10";
+            ResultSet rs5 = stmt.executeQuery(energyDanceabilityQ);
+            List<Stat> energiesdanceabilities = new ArrayList<>();
+            i = 0;
+            while (rs5.next()) {
+                i += 1;
+                Stat energydanceability = new Stat(i, rs5.getString("Track"), rs5.getString("Artist_Name"),
+                        rs5.getDouble("Energy_Danceability"));
+                energiesdanceabilities.add(energydanceability);
+            }
+            mv.addObject("Energy_Danceability", energiesdanceabilities);
+            
+            String energyDanceabilityAvgQ = "SELECT AVG((Energy + Danceability) / 2) as Mean_Energy_Danceability\r\n"
+                    + "  FROM Track JOIN Energy ON Track.Track_ID = Energy.Track_ID";
+            ResultSet rs6 = stmt.executeQuery(energyDanceabilityAvgQ);
+            rs6.next();
+            double energyDanceabilityAvg = rs6.getDouble("Mean_Energy_Danceability");
+            mv.addObject("EnergyDanceabilityAvg", energyDanceabilityAvg);
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect to the database!", e);
         }
