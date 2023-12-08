7fbb306b6b2a29b6ab6c5e9eda260df5a4a1d5ca
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2023-12-08 16:34:20 -0500

    Add files via upload

---
+++ Final_Project/final_project/src/main/java/com/Spotify/data/AllStats.java
@@ -0,0 +1,43 @@
+package com.Spotify.data;
+
+public class AllStats {
+    private String Track;
+    private String Artist;
+    private String Album;
+    private double Tempo;
+    private double Key;
+    private double Energy;
+    private double Danceability;
+    private double Valence;
+    private double Liveness;
+    private double Loudness;
+    private double Instrumentalness;
+    private double Speechiness;
+    private double Acousticness;
+
+    public AllStats(String Track, String Artist, String Album, double Tempo, double Key, double Energy, double Danceability,
+            double Valence, double Liveness, double Loudness, double Instrumentalness, double Speechiness,
+            double Acousticness) {
+        this.Track = Track;
+        this.Artist = Artist;
+        this.Album = Album;
+        this.Tempo = Tempo;
+        this.Key = Key;
+        this.Energy = Energy;
+        this.Danceability = Danceability;
+        this.Valence = Valence;
+        this.Liveness = Liveness;
+        this.Loudness = Loudness;
+        this.Instrumentalness = Instrumentalness;
+        this.Speechiness = Speechiness;
+        this.Acousticness = Acousticness;
+    }
+
+    @Override
+    public String toString() {
+        return "AllStats{" + "Track=" + Track + ", Artist=" + Artist + ", Album=" + Album + ", Tempo=" + Tempo + ", Key="
+                + Key + ", Energy=" + Energy + ", Danceability=" + Danceability + ", Valence=" + Valence
+                + ", Liveness=" + Liveness + ", Loudness=" + Loudness + ", Instrumentalness=" + Instrumentalness
+                + ", Speechiness=" + Speechiness + ", Acousticness=" + Acousticness + '}';
+    } // toString
+}
